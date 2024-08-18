c35a2cae351b0c08d582dbbf4d6e3dcbc3e49112
rexson21 <163307687+rexson21@users.noreply.github.com>
2024-08-18 12:00:44 +0530

    git commands

--- git-cmnd.sh
+++ git-cmnd.sh
@@ -22,4 +22,4 @@ git cherry-pick <NUMBER ID OF COMMIT> (to merge the branch from the current bran
 git log --oneline 
 git rebase <BRANCH NAME>
 git merge <BRANCH NAME>
-
+git remote show origin (display the path to push into repo)
