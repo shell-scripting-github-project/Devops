0502b0ff8d53d5bccd83603915669727158a3899
EC2 Default User <ec2-user@ip-172-31-18-139.ec2.internal>
2024-08-18 06:02:10 +0000

    testing

---
+++ git-cmnd.sh
@@ -0,0 +1,25 @@
+git clone
+export username
+export token
+helper function
+ls -la
+git status
+git add <filename>
+git log
+git diff
+git commit -m "YOUR MESSAGE"
+git reset --hard <NUMBERS> (version control)
+git init
+git -lrta
+git -a
+git checkout <filename> (to disguard changes in file after add)
+git remote -v (to check are we in git hub or not)
+git remote add "GIT HUB URL" (getting into the github)
+ssh-keygen -t rsa (to create a ssh public private key)
+git checkout -b <NEW BRANCH NAME> (to create a new branch from main branch)
+git checkout main (jumps to the main branch)
+git cherry-pick <NUMBER ID OF COMMIT> (to merge the branch from the current branch)
+git log --oneline 
+git rebase <BRANCH NAME>
+git merge <BRANCH NAME>
+
